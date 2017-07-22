f42b41525ddb177d248c6a5bdf0afd0bc83dfe8e
Jérôme Le Gal <jerome@joworld.net>
2017-07-22 22:25:40 +0200

    install: fix installation of Vim plugin syntastic

--- install.sh
+++ install.sh
@@ -3,13 +3,14 @@
 set -e
 set -u
 
+# Vim
 mkdir -p ~/.vim/bundle
 
 cd ~/.vim/bundle
 [ ! -d gruvbox ] && git clone https://github.com/morhetz/gruvbox
 [ ! -d vim-airline ] && git clone https://github.com/vim-airline/vim-airline
 [ ! -d nerdtree ] && git clone https://github.com/scrooloose/nerdtree.git
-[ ! -d vim-syntastic ] && git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+[ ! -d syntastic ] && git clone --depth=1 https://github.com/vim-syntastic/syntastic
 [ ! -d vim-easy-align ] && git clone https://github.com/junegunn/vim-easy-align
 
 cd $OLDPWD
@@ -19,3 +20,7 @@ for file in .vim/* .vimrc .zshenv .zshrc; do
 
     ln -fs $PWD/$file ~/$file;
 done
+
+# Zsh
+mkdir -p ~/.zsh
+[ ! -d ~/.zsh/zsh-git-prompt ] && git clone https://github.com/olivierverdier/zsh-git-prompt ~/.zsh/zsh-git-prompt
