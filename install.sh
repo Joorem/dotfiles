3b4e30e531e976789d2770dfbd8576cfdba40044
Jérôme Le Gal <jerome@joworld.net>
2017-07-22 21:20:24 +0200

    install.sh: ease the installation of configurations & plugins

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+set -u
+
+mkdir -p ~/.vim/bundle
+
+cd ~/.vim/bundle
+[ ! -d gruvbox ] && git clone https://github.com/morhetz/gruvbox
+[ ! -d vim-airline ] && git clone https://github.com/vim-airline/vim-airline
+[ ! -d nerdtree ] && git clone https://github.com/scrooloose/nerdtree.git
+[ ! -d vim-syntastic ] && git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+[ ! -d vim-easy-align ] && git clone https://github.com/junegunn/vim-easy-align
+
+cd $OLDPWD
+for file in .vim/* .vimrc .zshrc; do
+    [ -d ~/$file ] && rm -r ~/$file;
+    [ -f ~/$file ] && rm ~/$file;
+
+    ln -fs $PWD/$file ~/$file;
+done
