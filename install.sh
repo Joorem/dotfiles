c953977d28175bfaa4d105a8f340305ce7b88703
Jérôme Le Gal <jerome@joworld.net>
2020-03-19 00:46:43 +0530

    Shell: configure Fish
    - README.md has been updated
    - .vimrc: use a new plugin to enable Fish syntax
    - install.sh: deploy Fish configuration

--- install.sh
+++ install.sh
@@ -3,25 +3,48 @@
 set -e
 set -u
 
-# GPG
-mkdir -p ~/.gnupg
+#
+# Vars
+#
+ETC_FISH_FUNDLE=~/.config/fish/functions/fundle.fish
+ETC_VIM_PLUG=~/.vim/autoload/plug.vim
+ETC_ZSH_GITPROMPT=~/.zsh/zsh-git-prompt
+OPT_CURL=( --create-dirs --location --silent --output )
+URL_FISH_FUNDLE='https://raw.githubusercontent.com/danhper/fundle/master/functions/fundle.fish'
+URL_VIM_PLUG='https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+URL_ZSH_GITPROMPT='https://github.com/olivierverdier/zsh-git-prompt'
 
-# Vim
-mkdir -p ~/.vim_runtime/temp_dirs/undodir
-[ ! -f "$HOME/.vim/autoload/plug.vim" ] && curl --silent -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+#
+# Prerequisites
+#
+mkdir -p ~/.gnupg \
+  ~/.vim_runtime/temp_dirs/undodir \
+  ~/.zsh
 
-# Zsh
-mkdir -p ~/.zsh
-[ ! -d ~/.zsh/zsh-git-prompt ] && git clone --quiet https://github.com/olivierverdier/zsh-git-prompt ~/.zsh/zsh-git-prompt
+[ ! -f "$ETC_FISH_FUNDLE" ] && curl "${OPT_CURL[@]}" $ETC_FISH_FUNDLE $URL_FISH_FUNDLE
+[ ! -f "$ETC_VIM_PLUG" ] && curl "${OPT_CURL[@]}" $ETC_VIM_PLUG $URL_VIM_PLUG
+[ ! -d "$ETC_ZSH_GITPROMPT" ] && git clone --quiet $URL_ZSH_GITPROMPT $ETC_ZSH_GITPROMPT
 
-for file in .gnupg/* .gitconfig .vim/* .vimrc .zshenv .zshrc; do
+#
+# Symlinks
+#
+for file in .config/fish/* \
+  .gnupg/* \
+  .gitconfig \
+  .vim/* \
+  .vimrc \
+  .zshenv \
+  .zshrc; do
     [ -d "$HOME/$file" ] && rm -r "${HOME:?}/$file";
     [ -f "$HOME/$file" ] && rm "$HOME/$file";
 
     ln -fs "$PWD/$file" "$HOME/$file";
 done
 
+#
+# MacOS
+#
 if [[ "$OSTYPE" == "darwin"* ]]; then
     . install-macos.sh
 fi
