221ab30860d9cd43d8b205979fbe696d1dc7fcbc
Jérôme Le Gal <jerome@joworld.net>
2020-03-30 22:18:07 +0200

    Macos: fix typo caused by a bad C/C

--- install-macos.sh
+++ install-macos.sh
@@ -9,5 +9,5 @@ DIR_SUBLIME_TEXT=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 if [ -d "$DIR_SUBLIME_TEXT" ]; then
     cp resources/Preferences.sublime-settings "$DIR_SUBLIME_TEXT/Preferences.sublime-settings"
     cp resources/Default\ \(OSX\).sublime-keymap "$DIR_SUBLIME_TEXT/Default (OSX).sublime-keymap"
-    cp resources/Package\ Control.sublime-settings "$DIR_SUBLIME_TEXT/resources/Package Control.sublime-settings"
+    cp resources/Package\ Control.sublime-settings "$DIR_SUBLIME_TEXT/Package\ Control.sublime-settings"
 fi
