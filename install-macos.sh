a43811141535a572b688be60702a4b01e2cc0bf8
Jérôme Le Gal <jerome@joworld.net>
2020-11-06 14:38:58 +0100

    shellcheck: comestics improvements

--- install-macos.sh
+++ install-macos.sh
@@ -11,9 +11,9 @@ PRG_LAUNCHCTL=/bin/launchctl
 
 # Sublime Text
 if [ -d "${DIR_SUBLIME_TEXT}" ]; then
-    cp resources/Preferences.sublime-settings "$DIR_SUBLIME_TEXT/Preferences.sublime-settings"
-    cp resources/Default\ \(OSX\).sublime-keymap "$DIR_SUBLIME_TEXT/Default (OSX).sublime-keymap"
-    cp resources/Package\ Control.sublime-settings "$DIR_SUBLIME_TEXT/Package\ Control.sublime-settings"
+  cp resources/Preferences.sublime-settings "$DIR_SUBLIME_TEXT/Preferences.sublime-settings"
+  cp resources/Default\ \(OSX\).sublime-keymap "$DIR_SUBLIME_TEXT/Default (OSX).sublime-keymap"
+  cp resources/Package\ Control.sublime-settings "$DIR_SUBLIME_TEXT/Package\ Control.sublime-settings"
 fi
 
 if [ -d "${DIR_LAUNCH_AGENTS}" ]; then
--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ set -u
 #
 ETC_VIM_PLUG=~/.vim/autoload/plug.vim
 ETC_VIM_AIRLINE_GRUVBOX8=~/.vim/autoload/airline/themes/gruvbox8.vim
-OPT_CURL=( --create-dirs --location --silent --output )
+OPT_CURL=(--create-dirs --location --silent --output)
 URL_VIM_PLUG='https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 URL_VIM_AIRLINE_GRUVBOX8='https://gist.githubusercontent.com/lifepillar/cc3236a47bbba0261283f43f73074932/raw/511af9c8b630a38da94b5c31b0058f0339b8ea7a/gruvbox8.vim'
 
@@ -42,15 +42,15 @@ for file in \
   .vimrc \
   .zshenv \
   .zshrc; do
-    [ -d "$HOME/$file" ] && rm -r "${HOME:?}/$file";
-    [ -f "$HOME/$file" ] && rm "$HOME/$file";
+  [ -d "$HOME/$file" ] && rm -r "${HOME:?}/$file"
+  [ -f "$HOME/$file" ] && rm "$HOME/$file"
 
-    ln -fs "$PWD/$file" "$HOME/$file";
+  ln -fs "$PWD/$file" "$HOME/$file"
 done
 
 #
 # MacOS
 #
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    . install-macos.sh
+  . install-macos.sh
 fi
