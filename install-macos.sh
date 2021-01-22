506b0315fc085ec0ad1934b8993bada736c96ae3
Jérôme Le Gal <jerome@joworld.net>
2021-01-22 17:34:54 +0100

    vscode: handle settings.json

--- install-macos.sh
+++ install-macos.sh
@@ -6,6 +6,7 @@ set -u
 ETC_ARIA2C_SRV_NAME='net.joworld.aria2c'
 DIR_LAUNCH_AGENTS=~/Library/LaunchAgents
 DIR_SUBLIME_TEXT=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+DIR_VSCODE=~/Library/Application\ Support/Code/User
 PRG_ARIA2C=/usr/local/aria2/bin/aria2c
 PRG_LAUNCHCTL=/bin/launchctl
 
@@ -16,6 +17,10 @@ if [ -d "${DIR_SUBLIME_TEXT}" ]; then
   cp resources/Package\ Control.sublime-settings "$DIR_SUBLIME_TEXT/Package\ Control.sublime-settings"
 fi
 
+# VSCode
+ln -fs "$PWD/.config/vscode/settings.json" "$DIR_VSCODE/settings.json"
+
+# Aria2c
 if [ -d "${DIR_LAUNCH_AGENTS}" ]; then
   if [ ! -e "${DIR_LAUNCH_AGENTS}/${ETC_ARIA2C_SRV_NAME}.plist" ]; then
     cat <<EOF >"${DIR_LAUNCH_AGENTS}/${ETC_ARIA2C_SRV_NAME}.plist"
